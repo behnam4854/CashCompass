7054eb7f5b2e9902a8aadffcb06e1d63ab1ccb68
ibehnam <behnam4854@gmail.com>
2023-12-20 20:37:29 +0330

    added front-end

---
+++ app/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include,re_path
+from . import views
+from django.contrib.admin.views.decorators import staff_member_required
+
+
+app_name = 'intro'
+
+
+urlpatterns =[
+    path('dashboard/',views.welcome,name='dashboard'),
+]
--- app/app/settings.py
+++ app/app/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR), "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT= os.path.join(os.path.dirname(BASE_DIR), "media_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
